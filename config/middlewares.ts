8b30c1fcb6ae43fdb00e721d75206ccbaee79ea0
fakhrin31 <fakhrin3107@gmail.com>
2025-11-23 18:38:46 +0700

    fix(middlewares): update security and cors configurations
    - Add content security policy directives for connect-src and img-src
    - Configure CORS to allow frontend dev and production domains

--- config/middlewares.ts
+++ config/middlewares.ts
@@ -1,8 +1,28 @@
 export default [
   'strapi::logger',
   'strapi::errors',
-  'strapi::security',
-  'strapi::cors',
+  {
+    name: 'strapi::security',
+    config: {
+      contentSecurityPolicy: {
+        useDefaults: true,
+        directives: {
+          'connect-src': ['self', 'https:'],
+          'img-src': ['self', 'data:', 'blob:', 'https:'],
+        },
+      },
+    },
+  },
+  {
+    name: 'strapi::cors',
+    config: {
+      enabled: true,
+      origin: [
+        'http://localhost:5173',         // Frontend dev
+        'https://folearn.id', // Domain frontend produksi
+      ],
+    },
+  },
   'strapi::poweredBy',
   'strapi::query',
   'strapi::body',
